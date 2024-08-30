78180ec98a457e07693ec1e82fd7a1b466e897a5
PinkyJess <jessedazemabooks@gmail.com>
2024-08-30 13:19:01 +0100

    Add files via upload

---
+++ dictionary2.py
@@ -0,0 +1,23 @@
+grades={}
+grade['name'] ='Joy'
+grade['average']= 99
+if number >=90("Grade A"):
+    print{'Pass...'}
+grade['name']= 'Julia'
+grade['average']=89:
+elif number >=80(Grade B):
+    print{'Pass...'}
+grade['name'] ='Amanda'
+grade['average']=100:
+elif number >= 90("Grade A"):
+    print{'Pass...'}
+grade['name'] ='Chris Danjuma'
+grade['average']=89:
+else  number >=80("Grade B"):
+    print{'Pass...'}
+grade['name'] ='Chris '
+grade['average']=90:
+else  number >=90("Grade A"):        
+    print{'Pass...'}     
+del ['Chris Danjuma']
+print(grade) # Output: {'Joy','Julia','Amanda'}
